8c88f7e98324618c710d162dbe0e9b512d7679ae
aissasa <aissa.ben.zayed@gmail.com>
2022-12-31 17:11:12 +0100

    Initial Commit

--- PlaylistEditDetectionAndConversion.py
+++ PlaylistEditDetectionAndConversion.py
@@ -7,7 +7,8 @@ from CustomPlexConfig import CustomPlexConfig
 
 FREE_FILE_SYNC_LOGS_DIR = "D:/Music/MusicBee/Playlists/Logs"
 
-DEFAULT_PLEX_CONFIG = CustomPlexConfig("PlexServerDefaultConfig.json")
+JSON_DATA_FILE = os.path.join(os.path.dirname(__file__), "PlexServerDefaultConfig.json")
+DEFAULT_PLEX_CONFIG = CustomPlexConfig(JSON_DATA_FILE)
 
 DEFAULT_NVIDIA_SHIELD_MACHINE_ID = DEFAULT_PLEX_CONFIG.get("nvidia_shield_id")
 DEFAULT_NVIDIA_SHIELD_STORAGE_ROOT = "/storage/%s/" % DEFAULT_NVIDIA_SHIELD_MACHINE_ID
@@ -42,12 +43,12 @@ def mkdir_p(path):
 
 def get_newest_log_file(log_dir = FREE_FILE_SYNC_LOGS_DIR) -> str:
     if not os.path.exists(log_dir) or not os.path.isdir(log_dir):
-        logging.error("%s does not exist or is not a directory!", log_dir)
+        logging.error("%s does not exist or is not a directory!\n", log_dir)
         return None
     
     directory = os.listdir(log_dir)
     if len(directory) == 0:
-        logging.error("%s does not contain any log files!", log_dir)
+        logging.error("%s does not contain any log files!\n", log_dir)
         return None
     
     list_of_files = glob.glob(os.path.join(log_dir, '*.html'))
@@ -66,11 +67,11 @@ def extract_playlist_name_from_line(line) -> str:
     #result = re.search("quot;(.+\\\\)*(.+)\.m3u", line) #? this does not account for files that are not m3u
     result = re.search("quot;(.+\\\\)*(.+)\.(.+?)\&quot", line)
     if not result or result.lastindex < 2:
-        logging.error("Failed to extract playlist name from line: %s", line)
+        logging.error("Failed to extract playlist name from line: %s\n", line)
         return None
     
     if result.lastindex >= 3 and result.group(3) != EXPECTED_PLAYLIST_EXTENSION:
-        logging.warning("We have files that are not %s: %s%s.%s", EXPECTED_PLAYLIST_EXTENSION, result.group(1), result.group(2), result.group(3))
+        logging.warning("We have files that are not %s: %s%s.%s\n", EXPECTED_PLAYLIST_EXTENSION, result.group(1), result.group(2), result.group(3))
         return ""
     
     return result.group(2)
@@ -81,7 +82,7 @@ Not needed anymore.
 """
 def collect_playlists_from_log_file(log_file = get_newest_log_file()) -> tuple[list[str], list[str], list[str]]:
     if not os.path.exists(log_file) or not os.path.isfile(log_file):
-        logging.error("%s does not exist or is not a file!", log_file)
+        logging.error("%s does not exist or is not a file!\n", log_file)
         return None, None, None
     
     with open(log_file, mode="r", encoding="utf-8") as f:
@@ -94,7 +95,7 @@ def collect_playlists_from_log_file(log_file = get_newest_log_file()) -> tuple[l
                 created_playlist = extract_playlist_name_from_line(line)
                 assert created_playlist is not None
                 if created_playlist in created_playlists:
-                    logging.warning("%s has already been added to the created playslists!", created_playlist)
+                    logging.warning("%s has already been added to the created playslists!\n", created_playlist)
                 elif created_playlist != "":
                     created_playlists.add(created_playlist)
                 
@@ -102,7 +103,7 @@ def collect_playlists_from_log_file(log_file = get_newest_log_file()) -> tuple[l
                 updated_playlist = extract_playlist_name_from_line(line)
                 assert updated_playlist is not None
                 if updated_playlist in updated_playlists:
-                    logging.warning("%s has already been added to the updated playslists!", updated_playlist)
+                    logging.warning("%s has already been added to the updated playslists!\n", updated_playlist)
                 elif updated_playlist != "":
                     updated_playlists.add(updated_playlist)
                 
@@ -110,7 +111,7 @@ def collect_playlists_from_log_file(log_file = get_newest_log_file()) -> tuple[l
                 removed_playlist = extract_playlist_name_from_line(line)
                 assert removed_playlist is not None
                 if removed_playlist in removed_playlists:
-                    logging.warning("%s has already been added to the removed playslists!", removed_playlist)
+                    logging.warning("%s has already been added to the removed playslists!\n", removed_playlist)
                 elif removed_playlist != "":
                     removed_playlists.add(removed_playlist)
         
@@ -121,7 +122,7 @@ Converts playlists that are created locally in windows to a fomat that works for
 """
 def convert_playlist_for_plex(playlist_file_path, target_file_path):
     if not os.path.exists(playlist_file_path) or not os.path.isfile(playlist_file_path):
-        logging.error("%s does not exist or is not a file!", playlist_file_path)
+        logging.error("%s does not exist or is not a file!\n", playlist_file_path)
         return None
     
     converted_lines = []
@@ -134,7 +135,7 @@ def convert_playlist_for_plex(playlist_file_path, target_file_path):
                 converted_lines.append(converted_line)         
         target_file.writelines(converted_lines)
         
-    logging.debug("Converted %s to %s", playlist_file_path, target_file_path)
+    logging.debug("Converted %s to %s\n", playlist_file_path, target_file_path)
 
 """
 Testing ground
@@ -145,14 +146,12 @@ def main():
     newest_file = get_newest_log_file()
     print(newest_file)
     
-    print("")
-    print("--------- One line Playlist Extraction test --------------")
+    print("\n--------- One line Playlist Extraction test --------------")
     log_test_line = "<td>Updating file &quot;O:\Media\Music\Playlists\Latest\Best of the Best.m3u&quot;</td>"
     playlist_name = extract_playlist_name_from_line(log_test_line)
     print(playlist_name + "was extracted from %s" % log_test_line)
     
-    print("")
-    print("--------- Log file Playlists Extraction test --------------")
+    print("\n--------- Log file Playlists Extraction test --------------")
     created_playlists = set()
     updated_playlists = set()
     removed_playlists = set()
@@ -162,21 +161,19 @@ def main():
     removed_log_file = "D:\Music\MusicBee\Playlists\Logs\playlists_sync 2022-12-27 010938.038.html"
 
     created_playlists, updated_playlists, removed_playlists = collect_playlists_from_log_file(newest_file)
-    print("created playlists: ")
+    print("\nCreated playlists: ")
     print(created_playlists)
-    print("updated playlists: ")
+    print("\nUpdated playlists: ")
     print(updated_playlists)
-    print("removed playlists: ")
+    print("\nRemoved playlists: ")
     print(removed_playlists)
     
-    print("")
-    print("--------- One line convertion test --------------")
+    print("\n--------- One line convertion test --------------")
     test_line = "../../Library/Eminem/Eyo/Rivers.mp3"
     conversion_test = re.sub(REGEX_PATTERN_REPLACE_WITH_INTERNAL_DRIVE_PATH, DEFAULT_NVIDIA_SHIELD_MUSIC_DIR, test_line)
     print(test_line + "was converted to: " + conversion_test)
 
-    print("")
-    print("--------- Playlist conversion test --------------")
+    print("\n--------- Playlist conversion test --------------")
     orginal_playlist_path = DEFAULT_UNMODIFIED_PLAYLISTS_DIR + "Genres/Folk.m3u"
     target_playlist_path = DEFAULT_CONVERTED_PLAYLISTS_DIR + "Genres/Folk.m3u"
     convert_playlist_for_plex(orginal_playlist_path, target_playlist_path)
